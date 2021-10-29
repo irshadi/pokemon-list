8fad6f1d29b77aa056ac7fdb3e173f6a095f0da1
irshadi <irshadi@delman.io>
2021-10-29 11:18:51 +0700

    feat: create my pokemon, and pokemon details page

---
+++ pages/my_pokemons.js
@@ -0,0 +1,6 @@
+import { MyPokemons } from "../views/MyPokemon";
+import { Page } from "../components/Page";
+
+const MyPokemonPage = () => <Page page={MyPokemons} />;
+
+export default MyPokemonPage;
---
+++ pages/pokemon_details/index.js
@@ -0,0 +1,6 @@
+import { PokemonDetails } from "../../views/PokemonDetails";
+import { Page } from "../../components/Page";
+
+const PokemonDetailsPage = () => <Page page={PokemonDetails} />;
+
+export default PokemonDetailsPage;
